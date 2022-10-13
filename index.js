faa438d6afd9a20efb0819556eaeb22ad75587bf
Ricardo Carreira <R.Carreira@outlook.com>
2022-10-13 21:41:26 +0100

    making it work

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-import { getInput, setOutput, setFailed, setSecret, saveState } from '@actions/core';
-import { context } from '@actions/github';
+import { getInput, setOutput, setFailed } from '@actions/core';
 import { Octokit } from "octokit";
 
 try {
   // `who-to-greet` input defined in action metadata file
   const token = getInput('package-token');
+  
   const octokit = new Octokit({
     auth: `${token}`
   })
-  
-  const { data } = await octokit.request('GET /user/packages', { package_type: "nuget"});
 
-  console.log(`packages ${JSON.stringify(data, undefined, 2)}!`);
+  const  data = await octokit.request('GET /user/packages', { package_type: "nuget"});
+  const packagesNames = data.map(x => x.name)
 
-  const pack = getInput('package-name');
-  console.log(`Hello ${pack}!`);
-  const names = context.repo;
-  setOutput("names", JSON.stringify(names, undefined, 2));
-
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
-
+  setOutput("names", packagesNames);
 
 } catch (error) {
   
