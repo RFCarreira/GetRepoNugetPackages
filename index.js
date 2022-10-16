aebac19ec5554e740e781e885bb960a63adc8153
Ricardo Carreira <R.Carreira@outlook.com>
2022-10-16 20:08:15 +0100

    clean up

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { github } from "@actions/github"
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const token = getInput('token');
+  const token = getInput('github-token');
 
   const octokit = new Octokit({
     auth: `${token}`
